acd79d7764c708ee094b57b158bf58e0600968f8
EvaKadmon <adma_nog@hotmail.com>
2022-09-26 15:35:30 -0300

    Treino Java

---
+++ JS/termometro.js
@@ -0,0 +1,13 @@
+var temperatura = "12°"
+
+if (temperatura >= "38°") {
+console.log ("Você está com febre!");
+} 
+
+else if (temperatura <= "20°") {
+    console.log ("Você está morto!");
+}
+
+else {
+    console.log ("Nada de febre.");
+}
